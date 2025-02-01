5867dfd7fbe7908b09a8b56f5364ab266c1f7517
Harsh Anand <harsh25061999@gmail.com>
2025-02-01 22:47:47 +0530

    nodeJS server modified

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { express } from "express";
+const express = require('express');
 
 const app = express();
+const port = 3000;
 
-const PORT = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(PORT, () => {
-  console.log("Server is running on port  - ", PORT);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
